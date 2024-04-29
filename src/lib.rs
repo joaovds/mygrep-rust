9dab01193225da172a095698b1c6847b437d9601
João Victor da Silva <jv782063@gmail.com>
2024-04-29 13:45:22 -0300

    implementing the search_case_insensitive function

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
-use std::{error::Error, fs};
+use std::{env, error::Error, fs};
 
 pub struct Config {
     pub pattern: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -13,15 +14,26 @@ impl Config {
 
         let pattern = args[1].clone();
         let file_path = args[2].clone();
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        Ok(Config { pattern, file_path })
+        Ok(Config {
+            pattern,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.pattern, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.pattern, &contents)
+    } else {
+        search(&config.pattern, &contents)
+    };
+
+    for line in results {
         println!("{line}");
     }
 
