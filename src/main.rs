4288f62cbc59feea74bcc946785af5b90ac41957
João Victor da Silva <jv782063@gmail.com>
2024-04-29 12:55:24 -0300

    grouping configuration values

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,32 @@
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let pattern = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    let contents =
-        fs::read_to_string(file_path).expect(&format!("Error reading file {}.", file_path));
+    let contents = fs::read_to_string(config.file_path).expect("Error reading file.");
 
     println!("{contents}");
+}
+
+struct Config {
+    pattern: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Self, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let pattern = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { pattern, file_path })
+    }
 }
