82c0f29f360cc37444862cf86862393c10e26889
João Victor da Silva <jv782063@gmail.com>
2024-04-29 12:20:40 -0300

    reading file text

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let pattern = &args[1];
     let file_path = &args[2];
+
+    let contents =
+        fs::read_to_string(file_path).expect(&format!("Error reading file {}.", file_path));
+
+    println!("{contents}");
 }
